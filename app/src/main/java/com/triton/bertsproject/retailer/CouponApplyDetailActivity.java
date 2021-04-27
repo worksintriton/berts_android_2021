79a649eb92ea302ed2ef52b4a695e08a700094b5
Santhosh Kumar <santhoshvsk94@gmail.com>
2021-04-27 23:43:22 +0530

    Checkout screen

---
+++ app/src/main/java/com/triton/bertsproject/retailer/CouponApplyDetailActivity.java
@@ -0,0 +1,16 @@
+package com.triton.bertsproject.retailer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.triton.bertsproject.R;
+
+public class CouponApplyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon_apply_detail);
+    }
+}
